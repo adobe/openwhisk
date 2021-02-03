4c7dc0fc4d24e45b356114b07ebf6c87b4b007ae
Yevhen Sentiabov <sentiabo@adobe.com>
2021-02-03 17:00:01 -0600

    DEVX-564: Integrate Multitenant allocator to OW
     - Extracted headers and body from activation message and converted to Akka HTTP compatible format
     - Wrapped Akka HTTP response by WhiskActivation

--- core/controller/src/main/scala/org/apache/openwhisk/core/loadBalancer/TenantIsolatedLoadBalancer.scala
+++ core/controller/src/main/scala/org/apache/openwhisk/core/loadBalancer/TenantIsolatedLoadBalancer.scala
@@ -10,20 +10,22 @@ import org.apache.openwhisk.core.WhiskConfig.wskApiHost
 import org.apache.openwhisk.core.connector.{ActivationMessage, MessagingProvider}
 import org.apache.openwhisk.core.entity._
 import org.apache.openwhisk.spi.SpiLoader
-import spray.json.JsValue
+import spray.json.DefaultJsonProtocol._
+import spray.json.{JsValue, _}
 
+import java.time.Instant
 import scala.collection.immutable.Seq
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 class TenantIsolatedLoadBalancer(config: WhiskConfig,
                                  feedFactory: FeedFactory,
                                  controllerInstance: ControllerInstanceId,
-                                 implicit val messagingProvider: MessagingProvider = SpiLoader.get[MessagingProvider]
-                                )(
+                                 implicit val messagingProvider: MessagingProvider = SpiLoader.get[MessagingProvider])(
   implicit override val actorSystem: ActorSystem,
   logging: Logging,
   materializer: ActorMaterializer)
-  extends CommonLoadBalancer(config, feedFactory, controllerInstance) {
+    extends CommonLoadBalancer(config, feedFactory, controllerInstance) {
 
   override protected val invokerPool: ActorRef = actorSystem.actorOf(Props.empty)
 
@@ -52,42 +54,103 @@ class TenantIsolatedLoadBalancer(config: WhiskConfig,
    *         The future is guaranteed to complete within the declared action time limit
    *         plus a grace period (see activeAckTimeoutGrace).
    */
-  override def publish(action: ExecutableWhiskActionMetaData, msg: ActivationMessage)(implicit transid: TransactionId):
-  Future[Future[Either[ActivationId, WhiskActivation]]] = {
+  override def publish(action: ExecutableWhiskActionMetaData, msg: ActivationMessage)(
+    implicit transid: TransactionId): Future[Future[Either[ActivationId, WhiskActivation]]] = {
+    val whiskActivation: WhiskActivation = invoke(action, msg)
+    Future.successful(Future(Right(whiskActivation)))
+  }
+
+  /**
+   * Prepares activation message for needed format and calls HTTP Forwarder to invoke remote action.
+   *
+   * @param action: ExecutableWhiskActionMetaData
+   * @param msg: Activation Message
+   * @return WhiskActivation
+   */
+  private def invoke(action: ExecutableWhiskActionMetaData, msg: ActivationMessage): WhiskActivation = {
     val namespace: String = action.namespace.root.asString
     val pkg: String = if (action.namespace.defaultPackage) "default" else action.namespace.last.asString
     val act: String = action.name.asString
-
-    /**
-     * @TOOD there is should be a way how to get headers in more simple way
-     */
-    val headers: Seq[HttpHeader] = if (msg.content.get.fields.contains("__ow_headers")) msg.content.get.fields("__ow_headers").asJsObject.fields.map {
-      case (key: String, value: JsValue) => RawHeader(key, value.toString())
-    }.toIndexedSeq else Seq.empty[HttpHeader]
-
-    val body = if (msg.content.get.fields.contains("__ow_body")) msg.content.get.fields("__ow_body")
-    else msg.content.get.fields.filterKeys(elem => !elem.contains("__ow_"))
-
+    val headers: Seq[HttpHeader] = extractHeaders(msg.content)
+    val body: JsObject = extractBody(msg)
 
     /**
      * @TODO import Forwarder from multitenant allocator
      */
-//    val forwarder: Forwarder = new Forwarder(Settings(context.system), tenantRouter)
-//
-//    val httpResponse = forwarder.forward(namespace, pkg, act, ???, headers)
-    val httpResponse: Future[HttpResponse]= Future {???}
-    val response: Future[WhiskActivation] = Future {
-      // @TODO convert httpResponse to WhiskActivation
-      ???
+    val forwarder: Forwarder = new Forwarder(Settings(context.system), tenantRouter)
+    val whiskActivation: WhiskActivation = {
+      val start = Instant.now()
+      var activationResponse: ActivationResponse = ActivationResponse.success(Some(JsObject.empty))
+      val httpResponse: Future[HttpResponse] = forwarder.forward(namespace, pkg, act, body, headers)
+      httpResponse.onComplete {
+        case Success(res) => {
+          // @TODO check the status of HttpResponse future and set response object accordingly
+          val result: JsObject = res.entity.toJson.asJsObject
+          activationResponse = ActivationResponse.success(Some(result))
+        }
+        case Failure(exception) =>
+          activationResponse =
+            ActivationResponse(ActivationResponse.ApplicationError, Some(new JsString(exception.getMessage)))
+      }
+
+      WhiskActivation(
+        action.namespace,
+        action.name,
+        msg.user.subject,
+        msg.activationId,
+        start,
+        end = Instant.now(),
+        response = activationResponse)
     }
-    ???
+    whiskActivation
+  }
+
+  /**
+   * Extracts body from the activation message regardless of the action type and converts it to Json object.
+   *
+   * @param msg: ActivationMessage
+   * @return JsObject
+   */
+  private def extractBody(msg: ActivationMessage): JsObject = {
+    val body: JsObject =
+      if (msg.content.get.fields.contains("__ow_body")) msg.content.get.fields("__ow_body").asJsObject
+      else msg.content.get.fields.filterKeys(elem => !elem.contains("__ow_")).toJson.asJsObject
+    body
+  }
+
+  /**
+   * Extracts headers from activation message and converts them to Akka HTTP headers
+   *
+   * @param content: Option[JsObject]
+   * @return Seq[HttpHeader]
+   */
+  private def extractHeaders(content: Option[JsObject]): Seq[HttpHeader] = {
+
+    /**
+     * @TODO there is should be a way how to get headers in more simple way
+     */
+    val headers: Seq[HttpHeader] = {
+      if (content.get.fields.contains("__ow_headers"))
+        content.get
+          .fields("__ow_headers")
+          .asJsObject
+          .fields
+          .map {
+            case (key: String, value: JsValue) => RawHeader(key, value.toString())
+          }
+          .toIndexedSeq
+      else Seq.empty[HttpHeader]
+      headers
+    }
   }
 }
 
 object TenantIsolatedLoadBalancer extends LoadBalancerProvider {
   override def requiredProperties: Map[String, String] = ExecManifest.requiredProperties ++ wskApiHost
 
-  override def instance(whiskConfig: WhiskConfig, instance: ControllerInstanceId)
-                       (implicit actorSystem: ActorSystem, logging: Logging, materializer: ActorMaterializer): LoadBalancer =
+  override def instance(whiskConfig: WhiskConfig, instance: ControllerInstanceId)(
+    implicit actorSystem: ActorSystem,
+    logging: Logging,
+    materializer: ActorMaterializer): LoadBalancer =
     new TenantIsolatedLoadBalancer(whiskConfig, createFeedFactory(whiskConfig, instance), instance)
 }
