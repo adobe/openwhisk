659061682e3c3cf7de0d15046d9d39b202ef3218
Yevhen Sentiabov <sentiabo@adobe.com>
2021-03-02 17:07:09 -0600

    DEVX-656: Migrate Multi-allocator metrics to OW
     - Added metrics collection for Multi-tenant allocator

--- core/controller/src/main/scala/org/apache/openwhisk/core/loadBalancer/allocator/PrometheusCollectors.scala
+++ core/controller/src/main/scala/org/apache/openwhisk/core/loadBalancer/allocator/PrometheusCollectors.scala
@@ -1,10 +1,7 @@
 package org.apache.openwhisk.core.loadBalancer.allocator
 
-import java.time.Duration
-
 import kamon.Kamon
-import kamon.metric.{MeasurementUnit, Metric}
-import kamon.metric.Metric.Settings.ForValueInstrument
+import kamon.metric.Metric
 
 object PrometheusCollectors {
 
@@ -12,20 +9,12 @@ object PrometheusCollectors {
     Kamon.histogram("followerResponseTime", "Response time on the follower")
   val readerResponseTime: Metric.Gauge =
     Kamon.gauge("readerResponseTime", "Tenant allocation time on the reader")
-  val reallocationCounter: Metric.Counter = Kamon.counter(
-    "reallocationCounter",
-    "Counter for the reallocation messages received on leader",
-    ForValueInstrument(MeasurementUnit.none, Duration.ofMillis(500)))
   val versionNumberGauge: Metric.Gauge =
     Kamon.gauge("versionNumber", "Current Version Number of the distributed data key")
-  val unprocessedRequestsGauge: Metric.Gauge =
-    Kamon.gauge("unprocessedRequests", "Current number of unprocessed requests")
   val requestsCountCounter: Metric.Counter =
     Kamon.counter("requestsCounter", "Current number of processed requests on Leader")
   val readerErrorsCounter: Metric.Counter =
     Kamon.counter("readerErrorsCounter", "Number of errors on reader when requesting issuer")
-  val issuedRequestsCounter: Metric.Counter =
-    Kamon.counter("issuedRequestsCounter", "Current number of requests issued by followers")
   val readersRequestsCounter: Metric.Counter =
     Kamon.counter("readersRequestsCounter", "Current number of requests received by Readers")
   val requestProcessingGauge: Metric.Gauge =
@@ -40,8 +29,6 @@ object PrometheusCollectors {
     Kamon.counter(
       "allocationRedistributionsCounter",
       description = "Allocation requests resulting in redistributing non-empty slots")
-  val eventsCountAggregatorGauge: Metric.Gauge =
-    Kamon.gauge("eventsCountAggregatorGauge", "Request counts on Leader reported every sec")
   val tenantNumberGauge: Metric.Gauge =
     Kamon.gauge("tenantNumberGauge", "Current number of allocated tenants")
   val slotsNumberGauge: Metric.Gauge =
